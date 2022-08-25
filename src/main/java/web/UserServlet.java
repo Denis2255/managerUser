e1e711e94f0e274c197270e2d8b44858d7885861
Denis2255 <kdenny1998@mail.ru>
2022-08-25 12:26:28 +0300

    v1.02

--- src/main/java/web/UserServlet.java
+++ src/main/java/web/UserServlet.java
@@ -2,26 +2,21 @@ package web;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.List;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.UserDAO;
-import model.User;
+import model.Controller;
 
 
 @WebServlet("/")
 public class UserServlet extends HttpServlet {
 
-    private UserDAO userDAO;
+
 
     public void init() {
-        userDAO = new UserDAO();
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -32,26 +27,26 @@ public class UserServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String action = request.getServletPath();
-
+        Controller controller=new Controller();
         try {
             switch (action) {
                 case "/new":
-                    showNewForm(request, response);
+                    controller.showNewForm(request, response);
                     break;
                 case "/insert":
-                    insertUser(request, response);
+                    controller.insertUser(request, response);
                     break;
                 case "/delete":
-                    deleteUser(request, response);
+                    controller.deleteUser(request, response);
                     break;
                 case "/edit":
-                    showEditForm(request, response);
+                    controller.showEditForm(request, response);
                     break;
                 case "/update":
-                    editUser(request, response);
+                    controller.editUser(request, response);
                     break;
                 default:
-                    listUser(request, response);
+                    controller.listUser(request, response);
                     break;
             }
         } catch (SQLException e) {
@@ -59,63 +54,5 @@ public class UserServlet extends HttpServlet {
         }
     }
 
-    private void listUser(HttpServletRequest request, HttpServletResponse response)
-            throws SQLException, IOException, ServletException {
-        System.out.println("lustUser");
-        List<User> listUser = userDAO.selectAllUsers();
-        request.setAttribute("listUser", listUser);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("user-list.jsp");
-        dispatcher.forward(request, response);
-    }
 
-    private void showNewForm(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        System.out.println("showNewForm");
-        RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
-        dispatcher.forward(request, response);
-    }
-
-    private void showEditForm(HttpServletRequest request, HttpServletResponse response)
-            throws SQLException, ServletException, IOException {
-        System.out.println("showEditForm");
-        int id = Integer.parseInt(request.getParameter("id"));
-        User existingUser = userDAO.selectUser(id);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
-        request.setAttribute("user", existingUser);
-        dispatcher.forward(request, response);
-
-    }
-
-    private void insertUser(HttpServletRequest request, HttpServletResponse response)
-            throws SQLException, IOException {
-        System.out.println("insert");
-        String name = request.getParameter("name");
-        String email = request.getParameter("email");
-        String country = request.getParameter("country");
-        String phoneNumber = request.getParameter("phoneNumber");
-        String language = request.getParameter("language");
-        User newUser = new User(name, email, phoneNumber, language, country);
-        userDAO.addUser(newUser);
-        response.sendRedirect("list");
-    }
-
-    private void editUser(HttpServletRequest request, HttpServletResponse response)
-            throws SQLException, IOException {
-        int id = Integer.parseInt(request.getParameter("id"));
-        String name = request.getParameter("name");
-        String email = request.getParameter("email");
-        String country = request.getParameter("country");
-        String phoneNumber = request.getParameter("phoneNumber");
-        String language = request.getParameter("language");
-        User book = new User(id, name, email, phoneNumber, language, country);
-        userDAO.editUser(book);
-        response.sendRedirect("list");
-    }
-
-    private void deleteUser(HttpServletRequest request, HttpServletResponse response)
-            throws SQLException, IOException {
-        int id = Integer.parseInt(request.getParameter("id"));
-        userDAO.deleteUser(id);
-        response.sendRedirect("list");
-    }
 }
