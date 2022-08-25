05d908882818487a0ef0da55a0a01f49e10d0d6e
Denis2255 <kdenny1998@mail.ru>
2022-08-25 11:51:57 +0300

    v1.01

--- src/main/java/web/UserServlet.java
+++ src/main/java/web/UserServlet.java
@@ -61,7 +61,6 @@ public class UserServlet extends HttpServlet {
 
     private void listUser(HttpServletRequest request, HttpServletResponse response)
             throws SQLException, IOException, ServletException {
-        System.out.println("lustUser");
         List<User> listUser = userDAO.selectAllUsers();
         request.setAttribute("listUser", listUser);
         RequestDispatcher dispatcher = request.getRequestDispatcher("user-list.jsp");
@@ -70,14 +69,12 @@ public class UserServlet extends HttpServlet {
 
     private void showNewForm(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        System.out.println("showNewForm");
         RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
         dispatcher.forward(request, response);
     }
 
     private void showEditForm(HttpServletRequest request, HttpServletResponse response)
             throws SQLException, ServletException, IOException {
-        System.out.println("showEditForm");
         int id = Integer.parseInt(request.getParameter("id"));
         User existingUser = userDAO.selectUser(id);
         RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
@@ -88,7 +85,6 @@ public class UserServlet extends HttpServlet {
 
     private void insertUser(HttpServletRequest request, HttpServletResponse response)
             throws SQLException, IOException {
-        System.out.println("insert");
         String name = request.getParameter("name");
         String email = request.getParameter("email");
         String country = request.getParameter("country");
